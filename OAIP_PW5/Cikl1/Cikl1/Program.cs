06f449c3e967a7d1af9b7be259c418ba8509ae04
Xernoid <khabibrakhmanovb@yandex.ru>
2025-09-30 13:10:54 +0300

    vgshsh

---
+++ OAIP_PW5/Cikl1/Cikl1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cikl1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, n, sum;
+            n = 1;
+            sum = 0;
+            while (true)
+            {
+                sum += (n-1);
+                a = (sum / 5);
+                if (a==n)
+                {
+                    Console.WriteLine(n);
+                    break;
+                }
+                else
+                {
+                    n++;
+                    continue;
+                }
+            }
+            
+
+        }
+    }
+}
