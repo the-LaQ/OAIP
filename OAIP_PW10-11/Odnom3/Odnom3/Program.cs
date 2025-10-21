801d760b33c7b46a9060591302bcd262a7dde162
the-LaQ <steamkulikovan2008@gmail.com>
2025-10-21 11:06:28 +0300

    pw10-11 3

---
+++ OAIP_PW10-11/Odnom3/Odnom3/Program.cs
@@ -0,0 +1,23 @@
+﻿int n;
+Console.WriteLine("Сколько эл в масиве");
+n = Convert.ToInt32(Console.ReadLine());
+double[] a = new double[n];
+Random and = new Random();
+for (int i = 0; i < n; i++)
+{
+    a[i] = and.Next(-10, 10);
+    Console.WriteLine("\t" + a[i]);
+}
+for (int i = 0; i < n; i++)
+{
+    if (a[i] < 0)
+    {
+        a[i]=Math.Pow(a[i], 2);
+    }
+}
+Array.Sort(a);
+Console.WriteLine("новый масив");
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("\t" + a[i]);
+}
