e0c4fe29c99daadd49ec94de5ab81b7fea747848
the-LaQ <steamkulikovan2008@gmail.com>
2025-10-21 10:55:00 +0300

    pw10-11 2

---
+++ OAIP_PW10-11/Odnom2/Odnom2/Program.cs
@@ -0,0 +1,46 @@
+﻿try
+{
+
+    int n;
+    Console.WriteLine("Сколько эл в масиве");
+    n = Convert.ToInt32(Console.ReadLine());
+    double[] a = new double[n];
+    Random and = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = and.Next(-10, 10);
+        Console.WriteLine("\t" + a[i]);
+    }
+    double s;
+    int p1, p2;
+    s = 0;
+    p1 = 0;
+    p2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > 0)
+        {
+            p1 = i;
+            Console.WriteLine("Второй положительный эл:" + a[i]);
+            break;
+        }
+    }
+    for (int i = p1 + 1; i < n; i++)
+    {
+        if (a[i] > 0)
+        {
+            p2 = i;
+            Console.WriteLine("Первый положительный эл:" + a[i]);
+            break;
+        }
+    }
+    for (int i = p1 + 1; i < p2; i++)
+    {
+        s += a[i];
+    }
+    Console.WriteLine("Сумма между эл:" + s);
+}
+catch
+{
+    Console.WriteLine("Ошибка");
+}
